513b957e1462453146433dac12743eba1884ea60
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-01-16 14:13:18 +0530

    axios inatalled for managing api calls

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API = axios({
+  baseURL: "http://localhost:8080/api",
+});
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) req.headers.Authorization = `JWT ${token}`;
+  return req;
+});
+
+export default API;
